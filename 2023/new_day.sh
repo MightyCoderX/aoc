27cb9ec33a744f561f4efd7208ffe06da69edfc1
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2023-12-06 14:02:00 +0100

    Created script to generate day

---
+++ 2023/new_day.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Finds all days directories
+days=$(find . -type d -regextype egrep -regex '.*/[0-9]{2}' -printf '%f\n')
+last_day=$(echo "$days" | tail -1)
+new_day=$(echo $(( last_day + 1)) | xargs printf '%02d')
+
+echo "Creating day $new_day from template"
+
+mkdir "$new_day"
+cp -r ./00/* "$new_day"
+
+echo "Done"
+
