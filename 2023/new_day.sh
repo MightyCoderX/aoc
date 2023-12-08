5c903d404c8c2360124a39195c0cb121ee415b66
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2023-12-08 11:12:55 +0100

    Fixed copy

--- 2023/new_day.sh
+++ 2023/new_day.sh
@@ -8,7 +8,7 @@ new_day=$(echo $(( last_day + 1)) | xargs printf '%02d')
 echo "Creating day $new_day from template"
 
 mkdir "$new_day"
-cp -r ./00/* "$new_day"
+cp -r ./00/. "$new_day"
 
 echo "Done"
 
