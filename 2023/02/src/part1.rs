796b3767852fbc4739a90596cb5f732756666da1
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2023-12-08 23:49:32 +0100

    Welp stuck on part1 aren't I

--- 2023/02/src/part1.rs
+++ 2023/02/src/part1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::io::{self, prelude::*, BufReader};
 use std::fs::File;
@@ -8,15 +9,56 @@ pub fn main() -> io::Result<()>
     let file = File::open(path);
     let reader = BufReader::new(file.unwrap());
 
+    let mut sum = 0;
+
+    println!("Games: ");
+
     for line in reader.lines()
     {
         let line = line.unwrap();
         let len = line.len();
         let chars = line.chars();
-        let chars: Vec<char> = chars.collect();  
+        let chars: Vec<char> = chars.collect();
 
-        // Algorithm here
+        let (game, sets) = line.split_once(": ").unwrap();
+
+        let game_id = game.split_once(' ').unwrap().1;
+        let sets: Vec<_> = sets.split("; ").collect();
+
+        let mut color_limits = HashMap::from([
+            ("red", 12),
+            ("green", 13),
+            ("blue", 14)
+        ]);
+
+        
+        for set in sets
+        {
+            let color_count_pairs:Vec<_> = set.split(", ").collect();
+            println!("\t{}", color_count_pairs.len());
+
+            for pair in color_count_pairs
+            {
+                let (count, color) = pair.split_once(" ").unwrap();
+                
+                let count = count.parse::<i32>().unwrap();
+                
+                let current = color_limits.get(color).unwrap().to_owned();
+                
+                color_limits.insert(color, current-count);
+            }
+        }
+
+        if color_limits.iter().any(|x| x.1 < &0)
+        {
+            continue;
+        }
+
+        println!("game_id: {game_id} ");
+        sum += game_id.parse::<i32>().unwrap();
     }
+
+    println!("\nSum: {sum}");
 
     Ok(())
 }
