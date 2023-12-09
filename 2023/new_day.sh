cdd2101cc6c254ff98007fce95be5b5efa05f331
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2023-12-09 11:31:57 +0100

    Fixed script -.-

--- 2023/new_day.sh
+++ 2023/new_day.sh
@@ -2,7 +2,7 @@
 
 # Finds all days directories
 days=$(find . -type d -regextype egrep -regex '.*/[0-9]{2}' -printf '%f\n')
-last_day=$(echo "$days" | tail -1)
+last_day=$(echo "$days" | sort | tail -1)
 new_day=$(echo $(( last_day + 1)) | xargs printf '%02d')
 
 echo "Creating day $new_day from template"
