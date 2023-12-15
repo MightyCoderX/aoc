557f495cf07ed01c51a178e0751b16ddd14519f2
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2023-12-15 01:50:39 +0100

    Solving part 2 yeah...

--- 2023/03/src/part2.rs
+++ 2023/03/src/part2.rs
@@ -9,29 +9,89 @@ pub fn main() -> io::Result<()>
     let mut buf = String::new();
     let _ = file.read_to_string(&mut buf);
 
-    let lines: Vec<_> = buf.lines().collect();
-    let chars: Vec<_> = buf.chars().collect();
+    let lines: Vec<_> = buf.lines().map(|l| l.chars().collect::<Vec<_>>()).collect();
 
     let width = lines[0].len();
     let height = lines.len();
 
-    println!("{width}x{height}");
-
-    for (y, line) in buf.lines().enumerate()
+    let mut sum = 0;
+    
+    println!("{width}x{height}\n");
+    
+    for (y, line) in lines.iter().enumerate()
     {
-        
-        for (x, char) in buf.chars().enumerate()
+        for (x, char) in line.iter().enumerate()
         {
-            if char == '*' { continue }
+            let mut part_numbers = 0;
+            let mut ratio = 1;
+            if *char != '*' { continue }
+            
+            println!("------[GEAR]------");
+            
+            let top = y - ((y > 0) as usize);
+            let left = x - ((x > 0) as usize);
+            let bottom = y + ((y < height) as usize);
+            let right = x + ((x < width) as usize);
 
-            if let c = chars[y*(x -1)].is_ascii_digit()
+            println!("{} {} {} {}", top, left, bottom, right);
+            
+            for ry in top..=bottom
             {
-                
+                for rx in left..=right
+                {
+                    let char = lines[ry][rx];
+                    print!("{char}");
+                    
+                    if char.is_ascii_digit()
+                    {
+                        let mut buf = String::new();
+                        buf.push(char);
+                        
+                        let mut i = 1;
+                        
+                        while lines[ry][rx-i].is_ascii_digit()
+                        {   
+                            buf.insert(0, lines[ry][rx-i]);
+
+                            if rx-i == 0
+                            {
+                                break;
+                            }
+                            
+                            i+=1;
+
+                        }
+
+                        i = 1;
+                        while rx+i < width && lines[ry][rx+i].is_ascii_digit()
+                        {
+                            buf.push(lines[ry][rx+i]);
+                            i+=1;
+                        }
+                        println!("\nnum = {buf}");
+
+                        ratio *= buf.parse::<i32>().unwrap();
+                        part_numbers+=1;
+                        
+                        break;
+                    }   
+                }
+            }
+            
+            println!("-------------------");
+            
+            if part_numbers < 2
+            {
+                ratio = 0;
             }
 
+            println!("ratio: {ratio}\n");
 
+            sum += ratio;
         }
     }
+
+    println!("\n\nsum = {sum}");
 
     Ok(())
 }
