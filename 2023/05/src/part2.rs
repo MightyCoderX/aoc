a6ff95280a916682a23561b5089735353fa47a4c
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2023-12-16 00:08:34 +0100

    Initialized new day

---
+++ 2023/05/src/part2.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::io::{self, prelude::*};
+use std::fs::File;
+
+pub fn main() -> io::Result<()>
+{
+    let path = env::args().nth(1).unwrap();
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    let _ = file.read_to_string(&mut content);
+
+    
+
+    Ok(())
+}
